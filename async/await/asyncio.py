6a47197b3b804f9ac1816794d59cd6567fc2b37a
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-03-31 22:40:27 +0500

    async/await

---
+++ async/await/asyncio.py
@@ -0,0 +1,17 @@
+import asyncio
+
+async def task1():
+    print("Fun 1 started")
+    await asyncio.sleep(2)
+    print("Task 1 done")
+
+async def task2():
+    print("Fun 2 started")
+    await asyncio.sleep(2)
+    print("Task 2 done")
+
+async def main():
+    await asyncio.gather(task1(), task2())
+    
+
+asyncio.run(main())
